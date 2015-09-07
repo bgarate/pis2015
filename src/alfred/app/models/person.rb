0e41aa5d5d9d7707b2c73b67b5b8050287c42449
Oscar Montanes <omontanes@gmail.com>
2015-09-06 21:24:10 -0300

    Seguimos con modelos

--- src/alfred/app/models/person.rb
+++ src/alfred/app/models/person.rb
@@ -4,9 +4,13 @@ class Person < ActiveRecord::Base
   has_one :tech_rol
   has_many :participations
   has_many :projects, through: :participations
-  has_many :mentorships
+  has_many :mentorships, :foreign_key => "mentee_id"
+
+
+  has_many :mentee, through: :mentorships
   has_many :person_skills
   has_many :skills, through: :person_skills
 
 end
+
 
--- src/alfred/db/schema.rb
+++ src/alfred/db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905225815) do
+ActiveRecord::Schema.define(version: 20150906225655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "mentorships", force: :cascade do |t|
+    t.date     "init_date"
+    t.date     "finish_date"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "mentor_id"
+    t.integer  "mentee_id"
+  end
+
+  add_index "mentorships", ["mentee_id"], name: "index_mentorships_on_mentee_id", using: :btree
+  add_index "mentorships", ["mentor_id"], name: "index_mentorships_on_mentor_id", using: :btree
 
   create_table "milestones", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +40,20 @@ ActiveRecord::Schema.define(version: 20150905225815) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "notes", force: :cascade do |t|
+    t.text     "text"
+    t.integer  "visibility"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "participations", force: :cascade do |t|
+    t.date     "init_date"
+    t.date     "finish_date"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "people", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -36,7 +62,6 @@ ActiveRecord::Schema.define(version: 20150905225815) do
     t.date     "birth_date"
     t.date     "init_date"
     t.date     "finish_date"
-    t.string   "tech_rol"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
@@ -52,6 +77,12 @@ ActiveRecord::Schema.define(version: 20150905225815) do
   add_index "person_milestones", ["milestone_id"], name: "index_person_milestones_on_milestone_id", using: :btree
   add_index "person_milestones", ["person_id"], name: "index_person_milestones_on_person_id", using: :btree
 
+  create_table "person_skills", force: :cascade do |t|
+    t.date     "since_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string   "project_name"
     t.date     "init_date"
@@ -61,6 +92,14 @@ ActiveRecord::Schema.define(version: 20150905225815) do
     t.datetime "updated_at",   null: false
   end
 
+  create_table "skills", force: :cascade do |t|
+    t.string   "skill_name"
+    t.integer  "skill_type"
+    t.string   "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tech_rols", force: :cascade do |t|
     t.string   "tech_rol_name"
     t.string   "icon"
@@ -68,4 +107,13 @@ ActiveRecord::Schema.define(version: 20150905225815) do
     t.datetime "updated_at",    null: false
   end
 
+  create_table "technologies", force: :cascade do |t|
+    t.string   "tech_name"
+    t.string   "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "mentorships", "people", column: "mentee_id"
+  add_foreign_key "mentorships", "people", column: "mentor_id"
 end

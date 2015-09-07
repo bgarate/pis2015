bbce0389b769df6a1c4055a961336f0b8779a0e1
Diego Bortot <bortotdiegogm@gmail.com>
2015-09-07 10:46:13 -0500

    Relacion Mentorship doble loops funciona!!

--- src/alfred/app/models/person.rb
+++ src/alfred/app/models/person.rb
@@ -4,10 +4,21 @@ class Person < ActiveRecord::Base
   has_one :tech_rol
   has_many :participations
   has_many :projects, through: :participations
-  has_many :mentorships, :foreign_key => "mentee_id"
 
 
-  has_many :mentee, through: :mentorships
+  # mentees_asignations es una collection de <init_date, finish_date, mentee>
+  has_many :mentees_asignations, foreign_key: :mentor_id, class_name: "Mentorship"
+  # mentees es una collection de <mentee>
+  has_many :mentees, through: :mentees_asignations, source: :mentee
+
+
+  # mentors_asignations es una collection de <init_date, finish_date, mentor>
+  has_many :mentor_asignations, foreign_key: :mentee_id, class_name: "Mentorship"
+  # mentors es una collection de <mentor>
+  has_many :mentors, through: :mentor_asignations, source: :mentor
+
+
+
   has_many :person_skills
   has_many :skills, through: :person_skills
 

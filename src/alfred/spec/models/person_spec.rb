df87647ab6cf2277e1379e8cf3ca2b2d9f6b3981
Diego Bortot <bortotdiegogm@gmail.com>
2015-09-09 20:27:38 -0500

    Ahi van los primeros testes (ahora si)

--- src/alfred/spec/models/person_spec.rb
+++ src/alfred/spec/models/person_spec.rb
@@ -1,9 +1,31 @@
-require 'rspec'
+require 'rails_helper'
 
-describe 'My behaviour' do
+describe 'Person' do
 
-  it 'should do something' do
+  before do
+    @master = Person.new :name => 'Obiwan'
+    @padawan = Person.new :name => 'Look'
+    @project = Project.new :name => 'Equilibrar la fuerza'
+    @ms = Milestone.new :title => 'Destruir Death Star'
+    @techRole = TechRole.new :name => 'Jedi'
+    @skill = Skill.new :name => 'Mover piedras con la mente'
 
-    true.should == false
+    @master.mentees<<(@padawan)
+    @master.projects<<(@project)
+    @padawan.projects<<(@project)
+    @padawan.milestones<<(@ms)
+    @master.tech_role = @techRol
+    @master.skills<<(@skill)
+
+    @padawan.save!
+    @master.save!
+
+
+  end
+  it 'debería tener a Look como discípulo' do
+    expect(@master.mentees).to include(@padawan)
+  end
+  it 'debería tener a Obiwan como maestro' do
+    expect(@padawan.mentors).to include(@master)
   end
 end

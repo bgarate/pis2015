6a03032cffd25e7b42fa9fec0271fa936f9d7b89
Dario Cruz <dariodcruz@gmail.com>
2015-09-13 17:21:17 -0300

    Alfred 47: add_milestones_dates

--- src/alfred/app/controllers/person_controller.rb
+++ src/alfred/app/controllers/person_controller.rb
@@ -1,4 +1,12 @@
 class PersonController < ApplicationController
   def index
   end
+
+	def add_milestones_dates
+		#TODO: Debo checkear si soy mentor?
+		#TODO: Como consigo el milestone para setearle la fecha de fin?
+		
+		milestone = Milestone.find_by(id: params[:milestone_id])
+		milestone.update(due_date: params[:due_date]) if milestone
+	end
 end

a8659c77fecf8f3b50a21d0bab4ae041c7a44b37
sydz92 <sydz92@gmail.com>
2015-09-15 17:02:22 -0300

    Hitos pendientes y idioma arregaldo

--- src/alfred/config/application.rb
+++ src/alfred/config/application.rb
@@ -18,7 +18,7 @@ module Alfred
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :es
+    config.i18n.default_locale = :es
     # config.i18n.enforce_available_locales = false
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
--- src/alfred/db/seeds.rb
+++ src/alfred/db/seeds.rb
@@ -6,11 +6,50 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-p1 = Person.create!(name: "Bruno Garate", email: "bruno.garate@gmail.com", admin: true, start_date: 3.years.ago)
-p1.user = User.create!()
+tr = TechRole.new
+tr.name= "Vendedor de Tortas Fritas"
+tr.save!
 
-p2 = Person.create!(name: "Alfred Garate", email: "alfred.pis.2015@gmail.com", admin: true, start_date: 3.years.ago)
-p2.user = User.create!()
+per = Person.new
+per.name = 'Alfred'
+per.email = 'alfred.pis.2015@gmail.com'
+per.birth_date= Time.new(2012, 8, 29, 22, 35, 0)
+per.start_date= Time.new(2012, 8, 29, 22, 35, 0)
+per.tech_role = tr
+
+m = Milestone.new
+m.title = 'Conferencia Tecnológica'
+m.description= 'Se va a hablar de como las aspiradors roboticas van a cambiar nuestras vidas. Ademas de cafe y galletitas maria gratis'
+m.due_date= Time.now + (3*2*7*24*60*60)
+m.milestone_type= 1
+m.status=0
+per.milestones<<(m)
+
+m1 = Milestone.new
+m1.title = 'Entrega del prototipo de alfred'
+m1.description= 'Hay que entregar el protipo de alfred a la gente de pis. Ademas de cafe y galletitas maria gratis'
+m1.due_date= Time.now - (3*2*7*24*60*60)
+m1.status=0
+per.milestones<<(m1)
+
+sk1 = Skill.new
+sk1.name='angular'
+sk1.icon='skills/angular.png'
+sk2 = Skill.new
+sk2.name='java'
+sk2.icon='skills/java.png'
+per.skills<<(sk1)
+per.skills<<(sk2)
+
+pro = Project.new
+pro.name= 'Super Tortas 0.1'
+pro.start_date= Time.now - (2*7*24*60*60)
+pro.end_date= Time.now - (2*7*24*60*60)
+per.projects<<(pro)
+
+per.save!
+
+p1 = Person.create!(name: "Bruno Garate", email: "bruno.garate@gmail.com", admin: true, start_date: 3.years.ago)
 
 p1.milestones.create(title: "Un hito de prueba",
   description: "Esta es la descripción del hito",

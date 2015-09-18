6b4dd51c56efa7c1bed3d156622140beafc64058
gsiriani <guillebogus@hotmail.com>
2015-09-18 16:56:01 -0300

    Se agrega el campo status a la tabla projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -15,4 +15,6 @@ class Project < ActiveRecord::Base
   has_and_belongs_to_many :technologies
   has_many :participations
   has_many :people, through: :participations
+
+  enum status:[:active, :inactive, :finished]
 end

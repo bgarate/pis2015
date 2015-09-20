f6fb715d4bb098baad83130e92de21cd24891364
sydz92 <sydz92@gmail.com>
2015-09-20 14:02:50 -0300

    Correcion de bug en people#index, se pasa el id de person no el de user

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,7 +3,8 @@ class PeopleController < ApplicationController
   skip_before_action :admin?, only:[:show, :index, :me]
 
   def index
-      redirect_to action: 'show', id: session[:user_id]
+    u = User.find_by(id: session[:user_id])
+    redirect_to action: 'show', id: u.person_id
   end
 
   def me

ef77b00f6e0c3e7063337cfe78cbe00cd20d8fbe
gsiriani <guillebogus@hotmail.com>
2015-09-20 22:16:33 -0300

    Creacion funcionando, falta asignar tecnologias --skip-ci

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,7 +12,7 @@
 #
 
 class Project < ActiveRecord::Base
-  has_and_belongs_to_many :technologies
+  has_and_belongs_to_many :technologies, autosave: true
   has_many :participations
   has_many :people, through: :participations
 

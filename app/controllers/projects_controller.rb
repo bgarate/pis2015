01f5350aa4ff06d271c34bcbf7f4753d8a306fa7
gsiriani <guillebogus@hotmail.com>
2015-09-22 18:16:37 -0300

    Implementado edit project SIN edicion de tecnologias --skip-ci

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -29,6 +29,11 @@ class ProjectsController < ApplicationController
   end
 
   def update
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render :edit
+    end
   end
 
   def destroy

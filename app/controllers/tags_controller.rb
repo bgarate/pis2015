c3e39b6fcde6117d25e9612f4917cf241229904f
gonzalo.herrera <gonzalo.herrera.1993@gmail.com>
2015-09-26 20:50:40 -0300

    CRUD de Tag implementado, falla delete, faltan tests de coverage

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,59 @@
-class TagsController
+class TagsController < ApplicationController
+
+  before_action :get_tag, only: [:show, :edit, :update, :destroy]
+
+  def get_tag
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def show
+    if @tag
+      #nombre
+      @name = @tag.name
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    @tag.save
+    if @tag.valid?
+      flash.notice = "'Tag #{tag_params[:name]}' creado con éxito!"
+      redirect_to @tag
+    else
+      flash.alert = "'Tag #{tag_params[:name]}' no se ha podido crear"
+      redirect_to '/tags/new'
+    end
+  end
+
+  def update
+    if @tag.update(tag_params)
+      redirect_to @tag
+    end
+  end
+
+  def destroy
+    if @tag
+      @tag.destroy
+    end
+    redirect_to '/tags'
+  end
+
+  private
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+
+
 end

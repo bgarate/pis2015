32d9c2167acb539a1b2a8a436dcfe7c5a42db9d5
sydz92 <sydz92@gmail.com>
2015-10-07 23:43:47 -0300

    bugs alfred 69 y alfred 15

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -126,12 +126,7 @@ class MilestonesController < ApplicationController
     end
 
     @milestone.save!
-    #redirect_to @milestone
-    if session[:return_to]
-      redirect_to session[:return_to]
-    else
-      redirect_to root_path
-    end
+    redirect_to :back
   end
 
   def next_status_rej
@@ -142,12 +137,7 @@ class MilestonesController < ApplicationController
       @milestone.status= 'rejected'
     end
     @milestone.save!
-    #redirect_to @milestone
-    if session[:return_to]
-      redirect_to session[:return_to]
-    else
-      redirect_to root_path
-    end
+    redirect_to :back
   end
 
   def filter_note_by_visibility(note)

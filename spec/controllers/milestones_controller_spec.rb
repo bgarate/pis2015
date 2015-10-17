1782bd6c7e49e6abd224178082f6f5b408dd8df2
sydz92 <sydz92@gmail.com>
2015-10-16 22:58:18 -0300

    tests alfred 155

--- spec/controllers/milestones_controller_spec.rb
+++ spec/controllers/milestones_controller_spec.rb
@@ -272,6 +272,126 @@ describe MilestonesController, "Milestone Controller" do
 
     end
 
+    it 'creates a milestone with auto drive atach' do
+      a = double
+      allow(a).to receive(:push).with(anything()).and_return('')
+
+      f = double()
+      allow(f).to receive(:resource_id).and_return('unid')
+      allow(f).to receive(:title).and_return('untitulo')
+      allow(f).to receive(:human_url).and_return('/una/url')
+      allow(f).to receive(:copy).with(anything()).and_return(f)
+      allow(f).to receive(:acl).and_return(a)
+
+      s = double()
+      allow(s).to receive(:file_by_url).with(anything()).and_return(f)
+
+      GoogleDrive.stub(:login_with_oauth).with(anything()) { s }
+
+      session[:user_id] = @ad_user.id
+      p1 = Person.new
+      p1.name = "Juan Perez"
+      p1.email ="juanperez1@gmail.com"
+      p1.start_date =Time.now
+      p1.save!
+      p2 = Person.new
+      p2.name = "Juan2 Perez"
+      p2.email ="juanperez2@gmail.com"
+      p2.start_date =Time.now
+      p2.save!
+      p3 = Person.new
+      p3.name = "Juan3 Perez"
+      p3.email ="juanperez3@gmail.com"
+      p3.start_date =Time.now
+      p3.save!
+
+      c = Category.new :name=>'Feedback tecnico', doc_url: 'google/undoc', :icon=>'unicono'
+      c.created_at=Time.now
+      c.updated_at=Time.now
+      c.save!
+
+      get :new
+      post :create, :person_id=>@admin.id, :milestone=>{:title=>'milestone1', :description=>'unadescripcionde1',:category_id =>c.id},
+           :people=>[p1.id,p2.id,p3.id]
+      expect(response.status).to eq(302)
+
+    end
+
+    it 'creates a milestone with auto drive atach without permissions' do
+
+      s = double()
+      allow(s).to receive(:file_by_url).with(anything()) { raise Google::APIClient::ClientError }
+
+      GoogleDrive.stub(:login_with_oauth).with(anything()) { s }
+
+      session[:user_id] = @ad_user.id
+      p1 = Person.new
+      p1.name = "Juan Perez"
+      p1.email ="juanperez1@gmail.com"
+      p1.start_date =Time.now
+      p1.save!
+
+      c = Category.new :name=>'Feedback tecnico', doc_url: 'google/undoc', :icon=>'unicono'
+      c.created_at=Time.now
+      c.updated_at=Time.now
+      c.save!
+
+      get :new
+      post :create, :person_id=>@admin.id, :milestone=>{:title=>'milestone1', :description=>'unadescripcionde1',:category_id =>c.id},
+           :people=>[p1.id]
+      expect(response.status).to eq(302)
+    end
+
+    it 'creates a milestone with auto drive atach inavalid url 1' do
+
+      s = double()
+      allow(s).to receive(:file_by_url).with(anything()) { raise GoogleDrive::Error }
+
+      GoogleDrive.stub(:login_with_oauth).with(anything()) { s }
+
+      session[:user_id] = @ad_user.id
+      p1 = Person.new
+      p1.name = "Juan Perez"
+      p1.email ="juanperez1@gmail.com"
+      p1.start_date =Time.now
+      p1.save!
+
+      c = Category.new :name=>'Feedback tecnico', doc_url: 'google/undoc', :icon=>'unicono'
+      c.created_at=Time.now
+      c.updated_at=Time.now
+      c.save!
+
+      get :new
+      post :create, :person_id=>@admin.id, :milestone=>{:title=>'milestone1', :description=>'unadescripcionde1',:category_id =>c.id},
+           :people=>[p1.id]
+      expect(response.status).to eq(302)
+    end
+
+    it 'creates a milestone with auto drive atach inavalid url 2' do
+
+      s = double()
+      allow(s).to receive(:file_by_url).with(anything()) { raise URI::InvalidURIError }
+
+      GoogleDrive.stub(:login_with_oauth).with(anything()) { s }
+
+      session[:user_id] = @ad_user.id
+      p1 = Person.new
+      p1.name = "Juan Perez"
+      p1.email ="juanperez1@gmail.com"
+      p1.start_date =Time.now
+      p1.save!
+
+      c = Category.new :name=>'Feedback tecnico', doc_url: 'google/undoc', :icon=>'unicono'
+      c.created_at=Time.now
+      c.updated_at=Time.now
+      c.save!
+
+      get :new
+      post :create, :person_id=>@admin.id, :milestone=>{:title=>'milestone1', :description=>'unadescripcionde1',:category_id =>c.id},
+           :people=>[p1.id]
+      expect(response.status).to eq(302)
+    end
+
     it 'is valid with a title and description' do
       post :create, :person_id=>@admin.id, :milestone=>{:title=>'Milestone1', :description=>'unadescripciondemilestone', :due_date=>Time.now, :category_id=>@c1.id}
       expect(response.status).to eq(302)

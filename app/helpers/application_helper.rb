27f77a026d9717a3bc98c063e1b7b4ae9cf82844
omontanes <omontanes@gmail.com>
2015-10-26 21:27:14 -0300

    Icono

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,9 +13,6 @@ module ApplicationHelper
     link_to "", milestone_next_status_rej_path(milestone.id), method: :post, class: link_class, data: { confirm: "#{t('milestones.mark_as_sure')} #{t("milestones.#{milestone.get_next_status_rej_pend}")}?" }
   end
 
-  def user_admin(user, *options)
-    link_to "CAMBIAR", person_switch_admin_path(user.id), method: :post,  data: { confirm: "#{t('people.admin.mark_as_sure')}#{t("people.admin.#{user.get_next_admin_value}")}" }
-  end
 
 
 end

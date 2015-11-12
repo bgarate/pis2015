25df3e51361d3ac501f73dff007097e6d2c4d531
gsiriani <guillebogus@hotmail.com>
2015-11-12 17:33:07 -0300

    Se crea el controller de tecnologias

---
+++ app/controllers/technologies_controller.rb
@@ -0,0 +1,62 @@
+class TechnologiesController < ApplicationController
+
+  skip_before_action :admin?, only:[:show, :index]
+  before_action :get_technology, only:[:edit, :update, :destroy]
+
+  def get_technology
+    @technology = Technology.find_by(id: params[:id])
+  end
+
+  def index
+    @technologies = Technology.all.paginate(:page => params[:page], :per_page => 10).order('LOWER(name)')
+  end
+
+  def edit
+
+  end
+
+  def new
+
+  end
+
+  def show
+    redirect_to technologies_path
+  end
+
+  def create
+    @technology=Technology.new(technology_params)
+    @technology.save
+    if @technology.valid?
+      flash.notice = "#{technology_params[:name]} " + t('messages.create.success')
+    else
+      flash.alert = "#{technology_params[:name]} " + t('messages.create.error')
+    end
+    redirect_to technologies_path
+  end
+
+
+  def update
+    if @technology.update(technology_params)
+      flash.notice = "#{technology_params[:name]} " + t('messages.save.success')
+      redirect_to technologies_path
+    else
+      flash.alert = "#{technologies_path[:name]} " + t('messages.save.error')
+      render :edit
+    end
+  end
+
+  def destroy
+    if @technology
+      name = @technology.name
+      flash.notice = "#{name} " + t('messages.delete.success')
+    end
+    redirect_to technologies_path
+  end
+
+  private
+  def technology_params
+    params.require(:technology).permit(:name, :icon)
+  end
+
+
+end

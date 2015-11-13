6c95f2b9e63ffb5ac92b6f6fe3a69acd32ec97be
avasi <andresvasilev@gmail.com>
2015-11-13 17:00:23 -0200

    Se agrega orden por fechas en los hitos pendientes del dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,10 +12,11 @@ class DashboardController < ApplicationController
 
 
     if current_user_admin?
-      @milestones = Milestone.all.where('status = ?',Milestone.statuses[:pending]).order('LOWER(title)')
+      @milestones = Milestone.all.where('status = ?',Milestone.statuses[:pending]).order(highlighted: :desc, due_date: :asc, updated_at: :desc)
     else
       if (get_mentees_milestones(current_person)).length > 0
-        @milestones = get_mentees_milestones(current_person).where('status = ?',Milestone.statuses[:pending]).order('LOWER(title)')
+        #@milestones = get_mentees_milestones(current_person).where('status = ?',Milestone.statuses[:pending]).order('LOWER(title)')
+        @milestones = get_mentees_milestones(current_person).where('status = ?',Milestone.statuses[:pending]).order(highlighted: :desc, due_date: :asc, updated_at: :desc)
       else
         @milestones = get_mentees_milestones(current_person)
       end

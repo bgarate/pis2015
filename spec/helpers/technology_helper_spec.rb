8116a17040694987d30318cd9fd8b7c36b4dd94b
gsiriani <guillebogus@hotmail.com>
2015-11-13 17:43:38 -0300

    Se mejora test de cobertura de tecnologias

--- spec/helpers/technology_helper_spec.rb
+++ spec/helpers/technology_helper_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 require 'spec_helper'
 
-describe 'PersonHelper' do
+describe 'TechnologyHelper' do
   it 'Llamada con id' do
-    helper.person_picture("lfblntfejcpmmkh0wfny.jpg")
+    helper.technology_icon("lfblntfejcpmmkh0wfny.jpg")
     expect(response.status).not_to be_nil
   end
 
   it 'Llamada sin id' do
-    helper.person_picture(false)
+    helper.technology_icon(false)
     expect(response.status).not_to be_nil
   end
 end
